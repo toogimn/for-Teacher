5c10008f10fec585139e30c26ed6ff396fa30fb0
toogimn <togrulimanov06@gmail.com>
2025-03-12 22:53:59 +0400

    Delete ROTATE LİST.py

--- ROTATE LİST.py
+++
@@ -1,8 +0,0 @@
-def rotate_list(lst, k):
-    if not lst:
-        return lst
-    k = k % len(lst)
-    return lst[-k:] + lst[:-k]
-list=input("Enter the list: ").split()
-k=int(input("Enter the number of rotations:"))
-print(rotate_list(list,k))
